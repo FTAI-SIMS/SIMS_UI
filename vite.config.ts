d80d57a8040fc7880821434843ea198ae4116252
DrAbuhmida <mabrouka.abuhmida@pioneersconsultancygroup.co.uk>
2025-06-18 12:27:58 +0100

    comit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/FTAI_SIMS/' : '/',
+  base: mode === 'production' ? '/SIMS_UI/' : '/',
   server: {
     port: 3000,
   },
