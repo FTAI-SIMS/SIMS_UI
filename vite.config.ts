5ee89c5f62f556e5922edd91f18da3b5de09838f
DrAbuhmida <mabrouka.abuhmida@southwales.ac.uk>
2025-06-18 11:58:25 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: mode === 'production' ? '/FTAI_SIMS/' : '/',
   server: {
     port: 3000,
   },
-}); 
+})); 
